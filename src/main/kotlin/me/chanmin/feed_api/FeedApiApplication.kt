d0cec191d1665a2e4e045875f15cf2addf7e7b1a
Combi153 <ssrno009@gmail.com>
2026-01-07 15:29:38 +0900

    feat: initialize Spring Boot project with Gradle setup and basic configuration

---
+++ src/main/kotlin/me/chanmin/feed_api/FeedApiApplication.kt
@@ -0,0 +1,11 @@
+package me.chanmin.feed_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FeedApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FeedApiApplication>(*args)
+}
---
+++ src/test/kotlin/me/chanmin/feed_api/FeedApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.chanmin.feed_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FeedApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
