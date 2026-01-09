a65c2b8bf80e0228d524d33b8a5ce843e62be93f
Combi153 <ssrno009@gmail.com>
2026-01-09 17:35:16 +0900

    feat: add ProjectConfig for Kotest integration with Spring support

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // exposed kotlin
